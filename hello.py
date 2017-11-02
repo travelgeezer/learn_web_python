4267a7fcf4d0ba239205bf1fa08d317e087b2fca
travelgeezer <xxiaojiu9@gmail.com>
2017-11-02 07:02:55 +0000

    [T] try: update code

--- hello.py
+++ hello.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def hello():
     return 'hello world'
 
+@app.route('/item/<id>')
+def item(id):
+    return 'item: {}'.format(id)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000, debug=True)
---
+++ settings.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Hey, little fatty
